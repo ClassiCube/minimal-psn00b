4919e2ff6e3ced58fcebcb7fe1f02e2f631daa5f
UnknownShadow200 <unknownshadow200@gmail.com>
2024-06-01 17:46:35 +1000

    Add SDK

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu:22.04
+
+RUN apt-get update
+RUN apt-get -y install bash make
+
+RUN echo "## Exporting ps3dev environment" >> /root/.bashrc
+RUN echo "export PSNOOB=/usr/local/psnoob" >> /root/.bashrc
+RUN echo "export PATH=\$PATH:\$PSNOOB/bin" >> /root/.bashrc
+RUN echo "export PSN00BSDK_LIBS=$PSNOOB/lib/libpsn00b" >> /root/.bashrc
+
+COPY . /usr/local/psnoob
---
+++ lib/gcc/mipsel-none-elf/12.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-12.3.0/configure --prefix=/home/runner/work/PSn00bSDK/PSn00bSDK/gcc/linux --target=mipsel-none-elf --disable-docs --disable-nls --disable-werror --disable-libada --disable-libssp --disable-libquadmath --disable-threads --disable-libgomp --disable-libstdcxx-pch --disable-hosted-libstdcxx --enable-languages=c,c++ --without-isl --without-headers --with-float=soft --with-gnu-as --with-gnu-ld";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "float", "soft" } };
